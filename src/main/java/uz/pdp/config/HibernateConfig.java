0c9e31529ed4b8f6a92f9c9d8d2e095cba27cb8c
Javohir004 <javohirsharifjonov795@gmail.com>
2024-07-22 22:04:31 +0500

    sign inga harakat

--- src/main/java/uz/pdp/config/HibernateConfig.java
+++ src/main/java/uz/pdp/config/HibernateConfig.java
@@ -1,6 +1,4 @@
 package uz.pdp.config;
-
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
