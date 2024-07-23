98c9542306165ba0d9c49e6fca30abca3e99be02
SardorBoboyev <boboyevsardor350@gmail.com>
2024-07-23 23:45:48 +0500

    getAllAppointmentsOfUser method

--- src/main/java/uz/pdp/service/AppointmentService.java
+++ src/main/java/uz/pdp/service/AppointmentService.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Service;
 import uz.pdp.entity.Appointment;
 import uz.pdp.repository.AppointmentRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
-public class AppointmentService extends BaseService <Appointment, AppointmentRepository> {
+public class AppointmentService extends BaseService<Appointment, AppointmentRepository> {
 
     public AppointmentService(AppointmentRepository appointmentRepository) {
         repository = appointmentRepository;
     }
+
+
+    public List<Appointment> findAppointmentsByUser(UUID userId) {
+        return repository.findAppointmentsByUser(userId);
+    }
+
 
 }
