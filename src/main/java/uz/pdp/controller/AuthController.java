b8c244cf05c63773351032c00776a1371ca2dabc
Javohir004 <javohirsharifjonov795@gmail.com>
2024-07-25 12:11:14 +0500

    s

--- src/main/java/uz/pdp/controller/AuthController.java
+++ src/main/java/uz/pdp/controller/AuthController.java
@@ -39,10 +39,42 @@ public class AuthController {
         return "register";
     }
 
+    @RequestMapping("/balance")
+    public String balancePage(Model model, HttpSession session) {
+        User user = (User)session.getAttribute("user");
+        model.addAttribute("user", user);
+        model.addAttribute("balance", user.getBalance());
+        return "balance";
+    }
+
+    @RequestMapping("/back-balance")
+    public String backBalancePage(Model model, HttpSession session) {
+        User user = (User)session.getAttribute("user");
+        model.addAttribute("user", user);
+        model.addAttribute("balance", user.getBalance());
+        return "patient-page";
+    }
+
+    @PostMapping("add-balance")
+    public String addBalance(@RequestParam(name = "amount") double amount, Model model, HttpSession session){
+        User user = (User) session.getAttribute("user");
+        if(amount <= 0){
+            model.addAttribute("message", "Amount must be greater than 0");
+            model.addAttribute("balance", user.getBalance());
+            return "balance";
+        }
+
+        user.setBalance(user.getBalance() + amount);
+        userService.update(user);
+        model.addAttribute("balance", user.getBalance());
+        return "balance";
+    }
+
+
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute LoginDTO loginDto, HttpSession session) {
+    public String login(@ModelAttribute LoginDTO loginDto, HttpSession session, Model model) {
         User userEntity = userService.signIn(loginDto.username(), loginDto.password());
         session.setAttribute("user", userEntity);
         if (userEntity.getRole() == UserRole.PATIENT) {
@@ -52,6 +84,7 @@ public class AuthController {
         }else if(Objects.nonNull(userEntity.getRole())) {
             return "doctor-page";
         }
+        model.addAttribute("error", "Username or password incorrect");
         return "login";
     }
 
